ef1d21948e765628d58c135b345b91d8cece97e5
irorochad <wisdomchadere@gmail.com>
2023-06-28 03:30:15 +0100

    chore: continue install prettier, husky, and eslint configuration

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    tabWidth: 4,
+    printWidth: 80,
+    endOfLine: 'auto',
+    trailingComma: 'es5',
+    semi: true,
+    useTabs: false,
+    singleQuote: true,
+    bracketSpacing: true,
+};
