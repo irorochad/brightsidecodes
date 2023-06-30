be654611935acefabb91a74e32cee9bae01d3b4f
irorochad <wisdomchadere@gmail.com>
2023-06-30 12:46:22 +0100

    chore: continue install prettier, husky, & eslint configuration and fix code errors

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,106 +1,106 @@
-import React from 'react';
-import Head from 'next/head';
-import { GetStaticPropsContext, GetStaticPropsResult } from 'next';
-import { useRouter } from 'next/router';
-import { filter, asImageSrc, asText } from '@prismicio/client';
+import React from "react";
+import Head from "next/head";
+import { GetStaticPropsContext, GetStaticPropsResult } from "next";
+import { useRouter } from "next/router";
+import { filter, asImageSrc, asText } from "@prismicio/client";
 
-import { CMS_NAME } from '../../lib/constants';
-import { PostDocumentWithAuthor } from '../../lib/types';
-import { createClient } from '../../lib/prismic';
+import { CMS_NAME } from "../../lib/constants";
+import { PostDocumentWithAuthor } from "../../lib/types";
+import { createClient } from "../../lib/prismic";
 
-import Container from '../../components/container';
-import MoreStories from '../../components/more-stories';
-import PostBody from '../../components/post-body';
-import PostHeader from '../../components/post-header';
-import PostTitle from '../../components/post-title';
-import SectionSeparator from '../../components/section-separator';
+import Container from "../../components/container";
+import MoreStories from "../../components/more-stories";
+import PostBody from "../../components/post-body";
+import PostHeader from "../../components/post-header";
+import PostTitle from "../../components/post-title";
+import SectionSeparator from "../../components/section-separator";
 
 type PostProps = {
-  post: PostDocumentWithAuthor;
-  morePosts: PostDocumentWithAuthor[];
+    post: PostDocumentWithAuthor;
+    morePosts: PostDocumentWithAuthor[];
 };
 
 export default function Post({ post, morePosts }: PostProps) {
-  const router = useRouter();
+    const router = useRouter();
 
-  return (
-    <div>
-      <Container>
-        {router.isFallback ? (
-          <PostTitle>Fetching...</PostTitle>
-        ) : (
-          <>
-            <article>
-              <Head>
-                <title>
-                  {asText(post.data.title)} | Next.js Blog Page Example with{' '}
-                  {CMS_NAME}
-                </title>
-                <meta
-                  property="og:image"
-                  content={asImageSrc(post.data.cover_image, {
-                    width: 1200,
-                    height: 600,
-                    fit: 'crop',
-                  })}
-                />
-              </Head>
-              <PostHeader
-                title={post.data.title}
-                coverImage={post.data.cover_image}
-                date={post.data.date}
-                author={post.data.author}
-              />
-              <PostBody slices={post.data.slices} />
-            </article>
-            <SectionSeparator />
-            {morePosts && morePosts.length > 0 && (
-              <MoreStories posts={morePosts} />
-            )}
-          </>
-        )}
-      </Container>
-    </div>
-  );
+    return (
+        <div>
+            <Container>
+                {router.isFallback ? (
+                    <PostTitle>Fetching...</PostTitle>
+                ) : (
+                    <>
+                        <article>
+                            <Head>
+                                <title>
+                                    {asText(post.data.title)} | Next.js Blog
+                                    Page Example with {CMS_NAME}
+                                </title>
+                                <meta
+                                    property="og:image"
+                                    content={asImageSrc(post.data.cover_image, {
+                                        width: 1200,
+                                        height: 600,
+                                        fit: "crop",
+                                    })}
+                                />
+                            </Head>
+                            <PostHeader
+                                title={post.data.title}
+                                coverImage={post.data.cover_image}
+                                date={post.data.date}
+                                author={post.data.author}
+                            />
+                            <PostBody slices={post.data.slices} />
+                        </article>
+                        <SectionSeparator />
+                        {morePosts && morePosts.length > 0 && (
+                            <MoreStories posts={morePosts} />
+                        )}
+                    </>
+                )}
+            </Container>
+        </div>
+    );
 }
 
 export async function getStaticProps({
-  params,
-  previewData,
+    params,
+    previewData,
 }: GetStaticPropsContext<{ slug: string }>): Promise<
-  GetStaticPropsResult<PostProps>
+    GetStaticPropsResult<PostProps>
 > {
-  const client = createClient({ previewData });
+    const client = createClient({ previewData });
 
-  const [post, morePosts] = await Promise.all([
-    client.getByUID<PostDocumentWithAuthor>('post', params.slug, {
-      fetchLinks: ['author.name', 'author.picture'],
-    }),
-    client.getAllByType<PostDocumentWithAuthor>('post', {
-      fetchLinks: ['author.name', 'author.picture'],
-      orderings: [{ field: 'my.post.date', direction: 'desc' }],
-      predicates: [filter.not('my.post.uid', params.slug)],
-      limit: 2,
-    }),
-  ]);
+    const [post, morePosts] = await Promise.all([
+        client.getByUID<PostDocumentWithAuthor>("post", params.slug, {
+            fetchLinks: ["author.name", "author.picture"],
+        }),
+        client.getAllByType<PostDocumentWithAuthor>("post", {
+            fetchLinks: ["author.name", "author.picture"],
+            orderings: [{ field: "my.post.date", direction: "desc" }],
+            predicates: [filter.not("my.post.uid", params.slug)],
+            limit: 2,
+        }),
+    ]);
 
-  if (!post) {
+    if (!post) {
+        return {
+            notFound: true,
+        };
+    }
     return {
-      notFound: true,
+        props: { post, morePosts },
     };
-  }
-  return {
-    props: { post, morePosts },
-  };
 }
 
 export async function getStaticPaths() {
-  const client = createClient();
+    const client = createClient();
 
-  const allPosts = await client.getAllByType('post');
+    const allPosts = await client.getAllByType("post");
 
-  return {
-    paths: allPosts.map((post) => post.url),
-    fallback: true,
-  };
+    return {
+        paths: allPosts.map(post => post.url),
+        fallback: true,
+    };
 }
